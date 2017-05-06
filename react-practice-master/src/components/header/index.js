43c0cd47fcfac946e67d1a40ed8bac1c3435df79
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-05-06 22:09:05 +0300

    include contact component to home-page

--- react-practice-master/src/components/header/index.js
+++ react-practice-master/src/components/header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class Header extends React.Component {
     static path = '/';
@@ -10,6 +11,14 @@ export default class Header extends React.Component {
                     <div className='navbar-header'>
                         <a className='navbar-brand' href='#'>Brand</a>
                     </div>
+                    <ul className='nav navbar-nav'>
+                        <li>
+                            <Link to='/' >Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/contact' >Contact</Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         );
