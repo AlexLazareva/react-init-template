2682a630f7d0edeadebfabc260df5fb6693e2dca
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-05-07 21:39:14 +0300

    added setState to input

--- react-practice-master/src/pages/home/home.js
+++ react-practice-master/src/pages/home/home.js
@@ -4,15 +4,29 @@ import './styles.css';
 export default class HomePage extends React.Component {
     static path = '/';
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todoName: ''
+        };
+    }
+
+    inputOnChange(event) {
+        const todoName = event.target.value;
+        this.setState ({ todoName });
+    }
+
     render() {
+        const { todoName } = this.state;
         return (
-            <div className='row b-home'>
+            <div className='row-fluid b-home'>
                 <div className='col-xs-12'>
                     <ul>
                         <li>Item 1</li>
                     </ul>
                     <div className='col-xs-4'>
-                        <input type='text' className='form-control'/>
+                        <input type='text' className='form-control' value={ todoName } onChange={ this.inputOnChange.bind(this) }/>
                         <button className='btn btn-primary'>Add todo</button>
                     </div>
                 </div>
