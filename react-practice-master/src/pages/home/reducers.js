bc0423fab52eaac4155e998b77aeefd34dc594c8
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-05-08 16:48:28 +0300

    fixed small bag

--- react-practice-master/src/pages/home/reducers.js
+++ react-practice-master/src/pages/home/reducers.js
@@ -3,26 +3,27 @@ import { ADD_TODO } from './actions';
 const initialState = {
     todos: [
         {
-            id: '1',
+            id: 1,
             name: 'Todo 1'
         }
     ],
     error: ''
 };
+
 function homeReducer(state = initialState, action) {
-   switch (action.type) {
-       case ADD_TODO:
-           let todos = state.todos;
-           if (!action.error) {
-               todos.push({id: action.id, name: action.name});
-           }
-           return Object.assign({}, state, {
-               error: action.error,
-               todos
-           });
-       default:
-           return state;
-   }
+    switch (action.type) {
+        case ADD_TODO:
+            let todos = state.todos;
+            if (!action.error) {
+                todos.push({id: action.id, name: action.name});
+            }
+            return Object.assign({}, state, {
+                error: action.error,
+                todos
+            });
+        default:
+            return state;
+    }
 }
 
 const HomeReducer = {
