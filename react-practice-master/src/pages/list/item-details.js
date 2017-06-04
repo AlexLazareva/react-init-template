cfb0bb0378b966bb9e76dd156eddb1b38da1b595
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-06-04 20:31:24 +0300

    add ItemDetails routes

---
+++ react-practice-master/src/pages/list/item-details.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ItemDetails extends React.Component {
+    static path = '/';
+
+    render() {
+        return (
+            <div>
+                <h1>Item details works!</h1>
+            </div>
+        );
+    }
+}
