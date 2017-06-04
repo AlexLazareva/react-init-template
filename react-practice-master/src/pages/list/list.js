621d66fda472532ce1cf8d255cc08f6c0318b176
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-06-04 19:18:36 +0300

    create ListsRoutes and include to app

---
+++ react-practice-master/src/pages/list/list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ListPage extends React.Component {
+    static path = '/list';
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
---
+++ react-practice-master/src/pages/list/routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Route } from 'react-router';
+import ListPage from './list';
+
+export default (
+    <Route>
+        <Route component={ ListPage } path={ ListPage.path } />
+    </Route>
+);
