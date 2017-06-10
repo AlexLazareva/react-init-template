022164ca6032fcaf335aa47f3bffc9ae00ec534b
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-06-10 16:32:15 +0300

    add bootstrap classes to modal component

--- react-practice-master/src/components/modal/component.js
+++ react-practice-master/src/components/modal/component.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-export default class Modal extends React.Component {
+class Modal extends React.Component {
 
     render() {
         return (
-            <div>
-
+            <div className='modal fade'>
+                <div className='modal-dialog'>
+                    <div className='modal-content'>
+                        <div className='modal-header'>
+                            <h5 className='modal-title'>Modal title</h5>
+                            <button type='button' className='close'>
+                                <span>&times;</span>
+                            </button>
+                        </div>
+                        <div className='modal-body'>
+                            <p>Modal body text goes here.</p>
+                        </div>
+                        <div className='modal-footer'>
+                            <button type='button' className='btn btn-primary'>Save changes</button>
+                            <button type='button' className='btn btn-secondary'>Close</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
@@ -19,3 +35,4 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps())(Modal);
+
