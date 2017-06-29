057e517256025b0ba0b807b933afced293581d95
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-06-29 14:00:46 +0300

    create contact form

--- react-practice-master/src/pages/contact/contact.js
+++ react-practice-master/src/pages/contact/contact.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import { bindAll } from 'lodash';
+import Input from '../../components/ui/input/index';
 
 export default class ContactPage extends React.Component {
     
     static path = '/contact';
+
+    constructor(props) {
+        super(props);
+
+        bindAll(this, ['changeName', 'changeEmail', 'submit']);
+    }
+
+    changeName(name) {
+        this.setState( {name} );
+    }
+
+    changeEmail(email) {
+        this.setState( {email} );
+    }
+
+    submit(event) {
+        event.preventDefault();
+    }
     
     render() {
         return (
-            <div>
-                <div className="alert alert-danger">Hi</div>
-            </div>
+            <form className='b-contact'>
+                <h4>Имя: </h4>
+                <Input onChange={ this.changeName } value={ this.state.name } />
+                <h4>Email: </h4>
+                <Input onChange={ this.changeEmail } value={ this.state.email } />
+                <button type='submit' onClick={ this.submit }>Сохранить</button>
+            </form>
         );
     }
     
