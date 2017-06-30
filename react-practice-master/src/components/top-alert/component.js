129b72b7a9dd6977be428a000e187c7727061b86
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-06-30 14:20:00 +0300

    compliete TopAlert component and include styles

--- react-practice-master/src/components/top-alert/component.js
+++ react-practice-master/src/components/top-alert/component.js
@@ -2,20 +2,35 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { isEmpty } from 'lodash';
 
+import './styles.less';
+
 class TopAlert extends React.Component {
     static propTypes = {
-        dispatch: PropTypes.func.isRequired,
         user: PropTypes.object.isRequired
     };
+
+    removeMargin() {
+        document.querySelector('#app').classList.remove('m-t-50');
+    }
+
+    addMargin() {
+        document.querySelector('#app').classList.add('m-t-50');
+    }
 
     render() {
         const { name, email } = this.props.user;
 
-        if ( isEmpty(name) || isEmpty(email) ) return null;
+        if ( isEmpty(name) || isEmpty(email) ) {
+            this.removeMargin();
+            return null;
+        }
+
+        this.addMargin();
 
         return (
-            <div>
-                
+            <div className='alert alert-success b-alert'>
+                <span className='name'>{ name }</span>
+                <span className='email'>{ email }</span>
             </div>
         );
     }
